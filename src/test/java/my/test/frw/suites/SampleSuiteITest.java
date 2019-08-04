17f175f6796bd88bfa5d47ec77d19a83173d800d
sanju <sanju_by2004@yahoo.com>
2019-08-05 00:00:01 +0530

    added jenkins rest job details get test

--- src/test/java/my/test/frw/suites/SampleSuiteITest.java
+++ src/test/java/my/test/frw/suites/SampleSuiteITest.java
@@ -4,11 +4,14 @@ package my.test.frw.suites;
 import static org.testng.AssertJUnit.assertEquals;
 import static org.testng.AssertJUnit.assertNotNull;
 
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
 import org.openqa.selenium.lift.TestContext;
 import org.testng.Assert;
 import org.testng.ITestContext;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
+import io.restassured.RestAssured;
 
 import java.lang.reflect.Method;
 
@@ -28,10 +31,38 @@ public class SampleSuiteITest {
     }
 
     @Test
-    public void testCase3(ITestContext tCon) {
+    public void testJobDetails(ITestContext tCon) {
 
         System.out.println("Executing TC3");
-        Assert.assertTrue(true, tCon.getCurrentXmlTest().getName() + " : is failed");
+
+        // Specify the base URL to the RESTful web service
+
+        RestAssured.baseURI = "http://localhost:8080";
+
+        // Get the RequestSpecification of the request that you want to sent
+        // to the server. The server is specified by the BaseURI that we have
+        // specified in the above step.
+        RequestSpecification httpRequest = RestAssured.given();
+        httpRequest.auth().preemptive().basic("sanjaydub","Cybage123");
+
+        // Make a request to the server by specifying the method Type and the method URL.
+        // This will return the Response from the server. Store the response in a variable.
+        Response response = httpRequest.request(io.restassured.http.Method.GET, "/job/CT/api/json");
+//        httpRequest.request(io.restassured.http.Method.GET)
+
+        // Now let us print the body of the message to see what response
+        // we have recieved from the server
+        String responseBody = response.getBody().asString();
+        System.out.println("Response code is =>  " + response.getStatusCode());
+        System.out.println("Response Body is =>  " + responseBody);
+
+        System.out.println(response.print());
+
+
+
+
+
+        Assert.assertTrue(response.getStatusCode()==200, tCon.getCurrentXmlTest().getName() + " : is failed");
     }
 
 
